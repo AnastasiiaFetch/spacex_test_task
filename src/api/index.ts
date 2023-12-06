0b4cd2046b7e034b4df61986897ce8a0d1ebf247
Anastasiia Fetch <fetch.ana22@gmail.com>
2023-12-06 10:42:17 +0200

    api & providers added

---
+++ src/api/index.ts
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: 'https://spacex-production.up.railway.app/',
+  cache: new InMemoryCache(),
+});
+
+export const GET_All_CARDS = gql`
+  query GetCards {
+    rockets {
+      id
+      description
+      name
+    }
+  }
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { client } from './api';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <RecoilRoot>
+        <></>
+      </RecoilRoot>
+    </ApolloProvider>
+  </React.StrictMode>
+);
